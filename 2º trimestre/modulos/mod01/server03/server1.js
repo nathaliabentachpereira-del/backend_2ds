991fcbf093156bef4cc295d057da176ec76649bc
nathaliabentachpereira-del <nathalia.bentach.pereira@escola.pr.gov.br>
2026-08-17 14:26:01 +0000

    adiciona páginas HTML de erro, sobre e index; atualiza servidor para gerenciar rotas

--- 2º trimestre/modulos/mod01/server03/server1.js
+++ 2º trimestre/modulos/mod01/server03/server1.js
@@ -1,25 +1,33 @@
 const http = require('node:http')
-const path = require('node:http')
+const path = require('node:path')
 const fs = require('node:fs')
 const porta = 8005
+// Carrega o caminho dos arquivos
+const home = path.join(__dirname, 'pages/index.html')
+const sobre = path.join(__dirname, 'pages/sobre.html')
+const erro = path.join(__dirname, 'pages/erro.html')
 
+const server = http.createServer((req, res) =>{
+   // qual informação eu tenho?
+   const urlTratada = new URL (req.url, `http://${req.headers.host}`)//limpeza de url
+   const recurso = urlTratada.pathname // em qual rota estamos?
 
-const serve = http.createServer((req,res) => {
-    // qual informação eu tenho?
-    const urlTratada = new URL(req.url, `http://${req.headers.host}`)//limpeza de URL
-    const recurso = urlTratada.pathname // em qual rota estamos?
-
-    if(recurso === '/'){
-        res.statusCode = 200
+if(recurso === '/'){ // Página incial
+    res.statusCode = 200 
     res.setHeader('Content-Type','text/html; charset=utf-8')
-    res.end('Bem vindo(a)! \nHomepage \\o/')
-}else {
-    res.statusCode = 401
-    res.setHeader('Content-Type','text/plain; charset=utf-8')
-    res.end('481 não autorrizado')
+    return res.end(fs.readFileSync(home,'utf-8'))
+
+} else if(recurso === '/sobre'){
+    res.statusCode = 200 
+    res.setHeader('Content-Type','text/html; charset=utf-8')
+    return res.end(fs.readFileSync(sobre,'utf-8'))
+} 
+else {
+    res.statusCode = 404
+    res.setHeader('Content-Type','text/html; charset=utf-8')
+    res.end(fs.readFileSync(erro,'utf-8'))
 }
 })
-
-serve.listen(porta, ()=>{
+server.listen(porta, ()=>{
     console.log(`servidor rodando na porta ${porta}`)
 })
